07e3356d4a89c5502dfb7047c37f9fd713b3a043
tylerbare98 <tylerbare98@gmail.com>
2022-09-01 19:52:26 +0000

    Updating passing a prop with typescript

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,11 @@
 import { PropaneSharp } from '@mui/icons-material';
 import styles from './Product.module.css'
 
-// type productProps = {
-//   title: string, price: number, rating: number, imgSRC: string 
-// }
+type productProps = {
+  title: string, price: number, rating: number, imgSRC: string 
+}
 
-const Product: React.FC<{title: string, price: number, rating: number, imgSRC: string}> = (props) => {
+const Product: React.FC<productProps> = (props) => {
   return (
     <div className={styles.container}>
         <div className={styles.description}>
